81d29a286c8a72bc94b231dfb50d1b6f636a6da0
adit <aeka489@gmail.com>
2018-09-08 20:42:00 +0700

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+    state = {  }
+    render() {
+        return (
+            <div>
+                <h1>Footer</h1>
+            </div>
+        );
+    }
+}
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+    state = {  }
+    render() {
+        return (
+            <div>
+                <h1>Header</h1>
+            </div>
+        );
+    }
+}
