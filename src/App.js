6d937d5fdc20752ad2e00b5fecbb3dd5d4aaf1e3
adit <aeka489@gmail.com>
2018-09-08 23:27:27 +0700

    fix components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import {Header, Footer} from './components/layout';
+import {Header, Footer, Tabs, Card} from './components/layout';
+// import {Body} from 'native-base';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header/>
+        <Tabs></Tabs>
+        {/* <Sidebar></Sidebar> */}
+        <body>
+        <Card/>
+        <br/>
+        <Card/>
+        <Card/>
         <Footer/>
+        </body>
       </div>
     );
   }
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,6 +1,9 @@
 import Footer from './Footer';
 import Header from './Header';
+import Tabs from './Tabs'; 
+import Sidebar from './Sidebar';
+import Card from './Card'
 
 export{
-    Header, Footer
+    Header, Footer, Tabs, Sidebar, Card
 }
